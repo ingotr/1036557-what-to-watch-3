7c595844db4f67dd0eeccf5294fcc3354b0c35f4
Markovkin Aleksandr <markovkin88@gmail.com>
2020-02-19 09:01:03 +0300

    4.1.1: Добавляет ссылки на превью в моковые данные карточек фильмов

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -3,41 +3,57 @@ const films = [
     id: `mock-011`,
     title: `Fantastic Beasts: The Crimes of Grindelwald`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-012`,
     title: `Bohemian Rhapsody`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-013`,
     title: `Macbeth`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-014`,
     title: `Aviator`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-015`,
     title: `We need to talk about Kevin`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-016`,
     title: `What We Do in the Shadows`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-017`,
     title: `Revenant`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     id: `mock-018`,
     title: `Johnny English`,
     imgSrc: `img/aviator.jpg`,
+    previewSrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/
+      b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
 ];
 
