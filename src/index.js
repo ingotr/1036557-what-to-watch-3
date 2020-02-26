05af6104186c4fad6703531f83e46025614c3cea
Aleksandr <markovkin88@gmail.com>
2020-02-26 22:59:51 +0300

    5.1.7: Подключает Redux DevTools

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const FilmDetails = {
   RELEASE_DATE: 2014,
 };
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
