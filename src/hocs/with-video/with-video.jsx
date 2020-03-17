acc6de50620031773e23bec97fce61fbc8cb522f
Aleksandr <markovkin88@gmail.com>
2020-03-17 21:08:22 +0300

    6.1.1: Выносит состояние видео-плеера в HOC

---
+++ src/hocs/with-video/with-video.jsx
@@ -0,0 +1,101 @@
+import React, {createRef, PureComponent} from "react";
+import PropTypes from "prop-types";
+
+const MOVIE_OVER_TIMEOUT = 1000;
+
+const withVideo = (Component) => {
+  class WithVideo extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this._videoRef = createRef();
+
+      this.state = {
+        progress: 0,
+        isLoading: true,
+        isPlaying: props.isPlaying,
+        isMuted: true,
+      };
+    }
+
+    componentDidMount() {
+      if (!this._videoRef.current) {
+        return;
+      }
+
+      const videoRef = this._videoRef.current;
+
+      videoRef.className = `player__video--${videoRef.isPlaying ? `pause` : `play`}`;
+      videoRef.disabled = videoRef.isLoading;
+      videoRef.oncanplaythrough = () => this.setState({
+        isLoading: false,
+      });
+      videoRef.onplay = () => {
+        this.setState({
+          isPlaying: true,
+        });
+      };
+      videoRef.onpause = () => this.setState({
+        isPlaying: false,
+      });
+      videoRef.onMouseOver = () => {
+        setTimeout(this.setState({isPlaying: true}), MOVIE_OVER_TIMEOUT);
+      };
+      videoRef.onMouseOut = () => {
+        this.setState({isPlaying: false});
+      };
+      videoRef.poster = videoRef.src;
+      videoRef.muted = this.state.isMuted;
+    }
+
+    componentWillUnmount() {
+      const videoRef = this._videoRef.current;
+
+      videoRef.oncanplaythrough = null;
+      videoRef.onplay = null;
+      videoRef.onpause = null;
+      videoRef.ontimeupdate = null;
+      videoRef.src = ``;
+    }
+
+    componentDidUpdate() {
+      if (!this._videoRef.current) {
+        return;
+      }
+
+      const videoRef = this._videoRef.current;
+
+      if (this.state.isPlaying) {
+        videoRef.play();
+      } else {
+        videoRef.pause();
+        videoRef.load();
+      }
+    }
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+        >
+          <video
+            height={`175`}
+            width={`250`}
+            ref={this._videoRef}
+          />
+        </Component>
+      );
+    }
+  }
+
+  WithVideo.propTypes = {
+    isPlaying: PropTypes.bool.isRequired,
+    poster: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+  };
+
+  return WithVideo;
+};
+
+export default withVideo;
+
