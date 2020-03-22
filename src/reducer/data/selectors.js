7ccd36e785adbcc643b0ff44f62c23d3ce1a81e9
Aleksandr <markovkin88@gmail.com>
2020-03-23 01:11:19 +0300

    7.1.5: Создает список селекторов с помощью пакета reselect

---
+++ src/reducer/data/selectors.js
@@ -0,0 +1,23 @@
+import NameSpace from '../name-space.js';
+
+const NAME_SPACE = NameSpace.DATA;
+
+export const getMoviesByGenre = (state) => {
+  return state[NAME_SPACE].moviesByGenre;
+};
+
+export const getPromoMovie = (state) => {
+  return state[NAME_SPACE].movie;
+};
+
+export const getCurrentGenre = (state) => {
+  return state[NAME_SPACE].currentGenre;
+};
+
+export const getShowedMovies = (state) => {
+  return state[NAME_SPACE].showedMovies;
+};
+
+export const getMoviesCount = (state) => {
+  return state[NAME_SPACE].moviesCount;
+};
