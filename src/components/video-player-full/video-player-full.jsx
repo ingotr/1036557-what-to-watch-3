0dce14fabdce12cdaa5498a7ab50de08bb0c4f42
Aleksandr <markovkin88@gmail.com>
2020-03-22 12:24:48 +0300

    6.1.5: Меняет местами рендер кнопок play/pause в условном рендере

--- src/components/video-player-full/video-player-full.jsx
+++ src/components/video-player-full/video-player-full.jsx
@@ -93,14 +93,15 @@ class VideoPlayerFull extends PureComponent {
               }}
             >
               {this.state.isPlaying ? (
+                <><svg viewBox="0 0 14 21" width="14" height="21">
+                  <use xlinkHref="#pause"></use>
+                </svg><span>Pause</span></>
+              ) : (
                 <><svg viewBox="0 0 19 19" width="19" height="19">
                   <use xlinkHref="#play-s"></use>
                 </svg>
                   <span>Play</span></>
-              ) : (
-                  <><svg viewBox="0 0 14 21" width="14" height="21">
-                    <use xlinkHref="#pause"></use>
-                  </svg><span>Pause</span></>)}
+              )}
             </button>
             <div className="player__name">{this.props.film.name}</div>
 
