5be60f9d781b18062e3dbe2824876cd51ac94c04
Aleksandr <markovkin88@gmail.com>
2020-03-24 22:32:46 +0300

    7.2.3: В точке входа проверяет состояние пользователя. Авторизации нет по умолчанию.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,17 @@ import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 import reducer from './reducer/reducer.js';
 import {Operation as DataOperation} from './reducer/data/data.js';
+import {Operation as UserOperation,
+  ActionCreator, AuthorizationStatus} from './reducer/user/user.js';
 import createAPI from './api.js';
 import thunk from 'redux-thunk';
 
-const api = createAPI();
+const onUnauthorized = () => {
+  store.dispatch(
+      ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
@@ -20,6 +27,7 @@ const store = createStore(
 
 store.dispatch(DataOperation.loadPromoMovie());
 store.dispatch(DataOperation.loadMovies());
+store.dispatch(UserOperation.checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
