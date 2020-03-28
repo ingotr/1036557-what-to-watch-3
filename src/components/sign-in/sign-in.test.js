13162922a5e7d06ea3df80cb2b27901bab5ae53e
Aleksandr <markovkin88@gmail.com>
2020-03-29 01:09:28 +0300

    7.2.12: Обновляет тесты

--- src/components/sign-in/sign-in.test.js
+++ src/components/sign-in/sign-in.test.js
@@ -7,6 +7,9 @@ it(`Should render SignIn component`, () => {
     .create(
         <SignIn
           onSubmit={() => {}}
+          onHandleSubmit={() => {}}
+          loginError={false}
+          passwordError={false}
         />
     )
     .toJSON();
